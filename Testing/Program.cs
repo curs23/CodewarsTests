a9157de06919213a86a85a11a8b5558a47545620
Андрей Никифоров <curs@inbox.ru>
2020-10-21 23:19:57 +0300

    Add project files.

---
+++ Testing/Program.cs
@@ -0,0 +1,43 @@
+﻿using CodewarsTests;
+using Microsoft.VisualStudio.TestPlatform.ObjectModel;
+using System;
+using System.Linq;
+using System.Numerics;
+
+namespace Testing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "759";
+            Console.WriteLine(str);
+            IOrderedEnumerable<char> s = str.OrderByDescending(x => x);
+            
+            //string.Concat()
+            
+            
+            Console.WriteLine(str);
+            Console.ReadKey();
+
+
+
+
+
+            return;
+            do
+            {
+                Console.WriteLine("Введите число (q - выход):");
+                string numStr = Console.ReadLine();
+                if (numStr == "q") { break; }
+                else
+                {
+                    Console.WriteLine(Kata.DescendingOrder(int.Parse(numStr)));
+                    Console.WriteLine("----------");
+                }
+            } while (true);
+
+            //Console.ReadKey();
+        }
+    }
+}
